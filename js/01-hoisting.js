15d6ecf351739044f49e4f5086f82dab7eeaa8b4
Brijeshkumar Yadav <brijeshkumaryadav2026@gmail.com>
2024-10-08 09:31:07 +0530

    feat: hoisting > Update the notes

--- js/01-hoisting.js
+++ js/01-hoisting.js
@@ -1,18 +1,6 @@
 //! Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
 
-//! hoisting with traditional function
-
-// console.log(getName);
-// getName();
-// console.log(x);
-
-// var x = 10;
-
-// function getName() {
-//   console.log('Hello World');
-// }
-
-//! hoisting with arrow function
+// Hoisting with traditional function
 
 console.log(getName);
 getName();
@@ -20,6 +8,18 @@ console.log(x);
 
 var x = 10;
 
-var getName = () => {
+function getName() {
   console.log('Hello World');
-};
+}
+
+// Hoisting with arrow function -> Arrow function behave like variable
+
+// console.log(getName);
+// getName();
+// console.log(x);
+
+// var x = 10;
+
+// var getName = () => {
+//   console.log('Hello World');
+// };
